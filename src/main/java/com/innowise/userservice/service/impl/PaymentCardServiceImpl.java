f62c6f74b257eafe0187f588783a6a19d06a9d69
Gleb <glebelopov22@gmail.com>
2026-03-15 10:40:02 +0300

    feat: add Redis caching

--- src/main/java/com/innowise/userservice/service/impl/PaymentCardServiceImpl.java
+++ src/main/java/com/innowise/userservice/service/impl/PaymentCardServiceImpl.java
@@ -1,6 +1,8 @@
 package com.innowise.userservice.service.impl;
 
-import com.innowise.userservice.dto.*;
+import com.innowise.userservice.dto.CardCreateDto;
+import com.innowise.userservice.dto.CardShortDto;
+import com.innowise.userservice.dto.CardUpdateDto;
 import com.innowise.userservice.entity.PaymentCard;
 import com.innowise.userservice.entity.User;
 import com.innowise.userservice.exception.CardNotFoundException;
@@ -10,8 +12,11 @@ import com.innowise.userservice.mapper.PaymentCardMapper;
 import com.innowise.userservice.repository.PaymentCardRepository;
 import com.innowise.userservice.repository.UserRepository;
 import com.innowise.userservice.service.PaymentCardService;
-import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.Cache;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -27,9 +32,11 @@ public class PaymentCardServiceImpl implements PaymentCardService {
   private final PaymentCardRepository cardRepository;
   private final UserRepository userRepository;
   private final PaymentCardMapper cardMapper;
+  private final CacheManager cacheManager;
 
   @Override
   @Transactional
+  @CacheEvict(value = "userCards", key = "'cards::' + #userId")
   public CardShortDto createCard(Long userId, CardCreateDto dto) {
     if (!userRepository.existsById(userId)) {
       throw new UserNotFoundException(userId);
@@ -54,16 +61,15 @@ public class PaymentCardServiceImpl implements PaymentCardService {
   public CardShortDto getCardById(Long cardId) {
     PaymentCard card = cardRepository.findById(cardId)
             .orElseThrow(() -> new CardNotFoundException(cardId));
-
     return cardMapper.toShortDto(card);
   }
 
   @Override
+  @Cacheable(value = "userCards", key = "'cards::' + #userId")
   public List<CardShortDto> getCardsByUserId(Long userId) {
     if (!userRepository.existsById(userId)) {
       throw new UserNotFoundException(userId);
     }
-
     return cardRepository.findByUserId(userId)
             .stream()
             .map(cardMapper::toShortDto)
@@ -73,21 +79,35 @@ public class PaymentCardServiceImpl implements PaymentCardService {
   @Override
   @Transactional
   public CardShortDto updateCard(Long cardId, CardUpdateDto dto) {
+
     PaymentCard card = cardRepository.findById(cardId)
             .orElseThrow(() -> new CardNotFoundException(cardId));
-
+    Long userId = card.getUser().getId();
     cardMapper.updateFromDto(dto, card);
-
     PaymentCard updated = cardRepository.save(card);
+
+    evictUserCardsCache(userId);
+
     return cardMapper.toShortDto(updated);
   }
 
   @Override
   @Transactional
   public void changeCardActiveStatus(Long cardId, boolean active) {
-    if (!cardRepository.existsById(cardId)) {
-      throw new CardNotFoundException(cardId);
+    PaymentCard card = cardRepository.findById(cardId)
+            .orElseThrow(() -> new CardNotFoundException(cardId));
+    Long userId = card.getUser().getId();
+
+    card.setActive(active);
+    cardRepository.save(card);
+
+    evictUserCardsCache(userId);
+  }
+
+  private void evictUserCardsCache(Long userId) {
+    Cache cache = cacheManager.getCache("userCards");
+    if (cache != null) {
+      cache.evict("cards::" + userId);
     }
-    cardRepository.setActive(cardId, active);
   }
 }
--- src/main/java/com/innowise/userservice/service/impl/UserServiceImpl.java
+++ src/main/java/com/innowise/userservice/service/impl/UserServiceImpl.java
@@ -7,13 +7,15 @@ import com.innowise.userservice.dto.UserWithCardsDto;
 import com.innowise.userservice.entity.User;
 import com.innowise.userservice.exception.DuplicateUserException;
 import com.innowise.userservice.exception.UserNotFoundException;
-import com.innowise.userservice.repository.PaymentCardRepository;
+import com.innowise.userservice.mapper.UserMapper;
 import com.innowise.userservice.repository.UserRepository;
 import com.innowise.userservice.service.UserService;
-import com.innowise.userservice.mapper.UserMapper;
 import com.innowise.userservice.specification.UserSpecifications;
-import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.Cache;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
@@ -26,8 +28,8 @@ import org.springframework.transaction.annotation.Transactional;
 public class UserServiceImpl implements UserService {
 
   private final UserRepository userRepository;
-  private final PaymentCardRepository cardRepository;
   private final UserMapper userMapper;
+  private final CacheManager cacheManager;
 
   @Override
   @Transactional
@@ -43,29 +45,28 @@ public class UserServiceImpl implements UserService {
   }
 
   @Override
+  @Cacheable(value = "users", key = "'byId::' + #id")
   public UserWithCardsDto getUserById(Long id) {
     User user = userRepository.findByIdWithCards(id)
             .orElseThrow(() -> new UserNotFoundException(id));
-
     return userMapper.toWithCardsDto(user);
   }
 
   @Override
   public Page<UserShortDto> getAllUsers(String name, String surname, Pageable pageable) {
     Specification<User> spec = UserSpecifications.searchByNameAndSurname(name, surname);
-
     return userRepository.findAll(spec, pageable)
             .map(userMapper::toShortDto);
   }
 
   @Override
   @Transactional
+  @CacheEvict(value = "users", key = "'byId::' + #id")
   public UserShortDto updateUser(Long id, UserUpdateDto dto) {
     User user = userRepository.findById(id)
-            .orElseThrow(() -> new  UserNotFoundException(id));
+            .orElseThrow(() -> new UserNotFoundException(id));
 
     userMapper.updateFromDto(dto, user);
-
     User updated = userRepository.save(user);
     return userMapper.toShortDto(updated);
   }
@@ -74,8 +75,17 @@ public class UserServiceImpl implements UserService {
   @Transactional
   public void changeUserActiveStatus(Long id, boolean active) {
     if (!userRepository.existsById(id)) {
-      throw new  UserNotFoundException(id);
+      throw new UserNotFoundException(id);
     }
     userRepository.setActive(id, active);
+
+    evictUserCache(id);
+  }
+
+  private void evictUserCache(Long id) {
+    Cache cache = cacheManager.getCache("users");
+    if (cache != null) {
+      cache.evict("byId::" + id);
+    }
   }
 }
